b3fd429552ac3edfc00a8f817232ed04ec801041
Unknown <reneeseetyinghui@gmail.com>
2018-10-21 15:09:38 +0800

    Credit Card

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -15,63 +15,33 @@ import { MonoText } from '../components/StyledText';
 
 import  LineChartExample  from '../components/LineChart';
 
+import { Avatar, Button } from 'react-native-elements';
+
+import { CreditCardInput } from "react-native-credit-card-input";
+
 export default class HomeScreen extends React.Component {
   static navigationOptions = {
-    header: null,
   };
 
   render() {
     return (
       <View style={styles.container}>
         <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-
-              <LineChartExample></LineChartExample>
-
+          <View style={styles.my_container}>
+          <CreditCardInput onChange={this._onChange} />
+            <MonoText>Welcome Renee!</MonoText>
+            </View>
+            <View>
+            <MonoText>Bank Balance</MonoText>
+            <LineChartExample></LineChartExample>
+            <Button
+            title='Plan this month Budget!'
+             />
+            </View>
         </ScrollView>
-
-        <View style={styles.tabBarInfoContainer}>
-          <Text style={styles.tabBarInfoText}>This is a tab bar. You can edit it in:</Text>
-
-          <View style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-            <MonoText style={styles.codeHighlightText}>navigation/MainTabNavigator.js</MonoText>
-          </View>
-        </View>
       </View>
     );
   }
-
-  _maybeRenderDevelopmentModeWarning() {
-    if (__DEV__) {
-      const learnMoreButton = (
-        <Text onPress={this._handleLearnMorePress} style={styles.helpLinkText}>
-          Learn more
-        </Text>
-      );
-
-      return (
-        <Text style={styles.developmentModeText}>
-          Development mode is enabled, your app will be slower but you can use useful development
-          tools. {learnMoreButton}
-        </Text>
-      );
-    } else {
-      return (
-        <Text style={styles.developmentModeText}>
-          You are not in development mode, your app will run at full speed.
-        </Text>
-      );
-    }
-  }
-
-  _handleLearnMorePress = () => {
-    WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/guides/development-mode');
-  };
-
-  _handleHelpPress = () => {
-    WebBrowser.openBrowserAsync(
-      'https://docs.expo.io/versions/latest/guides/up-and-running.html#can-t-see-your-changes'
-    );
-  };
 }
 
 const styles = StyleSheet.create({
@@ -79,27 +49,15 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
   },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
+
+  my_container:{
+    justifyContent: 'center',
+    alignItems: 'center' 
+
   },
   contentContainer: {
     paddingTop: 30,
-  },
-  welcomeContainer: {
-    alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
-  },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
+  
   },
   getStartedContainer: {
     alignItems: 'center',
